13d2a196cce8e047598459405c308a5f59037857
NVauchok <nadezhda.m19@gmail.com>
2017-04-26 08:05:21 +0300

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿// Calculator. Console application in C#.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculate
+    {
+        static void Main()
+        {
+            double firstNumber;
+            double secondNumber;
+            string operation;
+            double result = 0;
+
+            Console.WriteLine("Please enter the first number");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the operation /, *, -, +");
+            operation = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("Division by zero is forbidden");
+                        break;
+                    }
+                    else
+                    {
+                        result = firstNumber / secondNumber;
+                        break;
+                    } 
+
+            }
+
+            Console.WriteLine("Result = {0}", result.ToString());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
